dfc0a421c7c834c9b9932870f4652eee11f435d0
HusseinElsaka <52578649+HusseinElsaka@users.noreply.github.com>
2022-02-08 10:40:37 +0200

    This is the EX1 HW1

---
+++ Unit 2/Lesson 2.C BASIC/Homework 1/1.C Program to Print a Sentence/C Program to Print a Sentence.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : C Program to Print a Sentence
+ Author      : Hussein Elsaka
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("C Programming");
+	return 0;
+}
