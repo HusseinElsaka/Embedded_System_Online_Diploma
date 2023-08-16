f915681dceb4d0778c072ea001b01891ada807b4
HusseinElsaka <ehussein366@gmail.com>
2023-08-16 03:31:48 +0300

    Student Data Base
    This is Base on Linkedlist

---
+++ Unit 4/FIFO - LIFO - Linkedlist/StudentDataBase/Linkedlist_Cfg.h
@@ -0,0 +1,21 @@
+
+#ifndef LINKEDLIST_CFG_H_
+#define LINKEDLIST_CFG_H_
+
+
+
+// typedef unsigned int List_Element_Type ;
+
+
+
+struct SStudent
+{
+	int recordNumber;
+	int ID;
+	char name[40];
+	float height;
+};
+
+typedef struct SStudent List_Element_Type ;
+
+#endif /* LINKEDLIST_CFG_H_ */
