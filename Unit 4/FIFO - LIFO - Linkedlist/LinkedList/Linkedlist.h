a0a6580d356738cab36c7d879b3af600e3feb715
HusseinElsaka <ehussein366@gmail.com>
2023-08-16 03:20:03 +0300

    Linkedlist Node

--- Unit 4/FIFO - LIFO - Linkedlist/LinkedList/Linkedlist.h
+++ Unit 4/FIFO - LIFO - Linkedlist/LinkedList/Linkedlist.h
@@ -3,4 +3,29 @@
 #ifndef LINKEDLIST_H_
 #define LINKEDLIST_H_
 
+#include <stdlib.h>
+#include "Linkedlist_Cfg.h"
+
+
+typedef struct {
+	List_Element_Type entry;
+	struct SDataNode *next;
+}SDataNode;
+
+typedef struct {
+	SDataNode *head;
+	int size;
+}LinkedList_t;
+
+typedef enum
+{
+	LINKEDLIST_NO_ERROR,
+	LINKEDLIST_NULL,
+	LINKEDLIST_MEMORY_ERROR,
+	LINKEDLIST_EMPTY,
+	LINKEDLIST_INNER_LOOP,
+	LINKEDLIST_NO_INNER_LOOP,
+	LINKEDLIST_OUT_OF_INDEX
+}Linkedlist_statue;
+
 #endif /* LINKEDLIST_H_ */
